12fb3d5450c0081587e51f473932bb6ec7fc6db1
John Corn <jcornacchioli@gmail.com>
2023-05-20 11:34:03 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const giftsGiverName = ["teddy bear", "drone", "doll"];
+const thankYouMessages = [];
+
+function writeCards(giftsGiverName, giftName)
+{
+    for (let i = 0; i < giftsGiverName.length; i++)
+        thankYouMessages.push(`Thank you, ${giftsGiverName[i]}, for the wonderful ${giftName} gift!`);
+
+    return thankYouMessages;
+}
+
+function countDown(index)
+{
+    for (let i = index; i >= 0; i--)
+        console.log(i);
+}
